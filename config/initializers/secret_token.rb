556710251cbdfd747e51fc988f8d3f0c3c5e13c9
John Wallace <jdwallace12@gmail.com>
2014-05-06 20:55:57 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JohnWallaceSinglePage::Application.config.secret_key_base = 'cc8f8020b5bb1d089890f1cd2d6426c5d215cd1321d0edf87df77f23207a1f02ac943cf517787a6f905731e17b63fd08883ecb2e5b55646c543e1f85616cdf88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JohnWallaceSinglePage::Application.config.session_store :cookie_store, key: '_john_wallace_single_page_session'
